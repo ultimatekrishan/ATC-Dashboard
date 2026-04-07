8da6e62eba89093ac8ca3ed29bbfee0d78dcebe5
Krish Kumar <ultimatekrishan@gmail.com>
2026-04-07 17:37:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@
  * Paste your Google Apps Script Web App URL here after deployment.
  * See SETUP_GUIDE.html for step-by-step instructions.
  */
-const APPS_SCRIPT_URL = 'YOUR_APPS_SCRIPT_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzLp2b3m6tAwDkQm61iEuE-DfL8jyjHmBscb7fMdZQmeZkETAkAnv4Mlgx0IySbSUx34w/exec';
 
 /**
  * Role permissions map.
